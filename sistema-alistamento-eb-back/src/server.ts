414fc5b6e6a0b5d8be25bbe807733e19e3c4641c
Augusto <augstarachrafael@gmail.com>
2020-08-16 18:47:42 -0300

    Jest ESLint config

--- sistema-alistamento-eb-back/src/server.ts
+++ sistema-alistamento-eb-back/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-const db = require("./database/index");
+
 // import routes from "./routes/routes";
 
 const app = express();
@@ -8,7 +8,3 @@ app.use(express.json());
 // app.use('/api', routes);
 
 app.listen(3000, () => console.log("Server started at http://localhost:3000"));
-
-db.select()
-  .from("tipo_pergunta")
-  .then((data: any) => console.log(data));
