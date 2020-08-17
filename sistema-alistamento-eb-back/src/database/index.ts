82e16c294ec6ac610db90fd0467a8b44fef61ad0
Augusto <augstarachrafael@gmail.com>
2020-08-16 23:32:49 -0300

    Knexjs configurado

--- sistema-alistamento-eb-back/src/database/index.ts
+++ sistema-alistamento-eb-back/src/database/index.ts
@@ -1,12 +1,10 @@
-const knex = require("knex")({
-  client: "pg",
-  version: "12.3",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  },
+require('dotenv').config();
+
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: process.env.DB_STRING,
 });
 
-module.exports = knex;
+export default db;
--- sistema-alistamento-eb-back/src/server.ts
+++ sistema-alistamento-eb-back/src/server.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import express from 'express';
 
+import db from './database/index';
 // import routes from "./routes/routes";
 
+const data = db.select().from('TIPO_PERGUNTA').then((result: any) => {
+  console.log(result);
+}).catch((err: any) => {
+  console.log(err);
+});
 const app = express();
 
 app.use(express.json());
 // app.use('/api', routes);
 
-app.listen(3000, () => console.log("Server started at http://localhost:3000"));
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+
+console.log(data);
